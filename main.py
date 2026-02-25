dd06209518a1abaeca008690ca7a983ca68a52e9
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 15:23:49 +0900

    pydantic 모델 기본

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/items/")
-def read_items(skip: int = 0, limit: int = 10):
-    return {"skip": skip, "limit": limit}
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 0.1
 
 
 @app.post("/items/")
-def create_item(item: dict):
-    return {"item": item}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: dict):
-    return {"item_id": item_id, "updated_item": item}
-
-
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    return {"message": f"Item {item_id} has been deleted"}
+async def create_item(item: Item):
+    return {"item": item.dict()}
