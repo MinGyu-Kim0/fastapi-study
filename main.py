61a835d5528d00e3926971843fb8d8eb0584e7a9
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 17:41:51 +0900

    Query 클래스 다양한 옵션

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ def read_items(internal_query: str = Query(None, alias="search")):
 @app.get("/users2/")
 def read_users(q: str = Query(None, deprecated=True)):
     return {"q": q}
+
+
+@app.get("/items2/")
+def read_items(
+    string_query: str = Query(
+        default="default value",
+        min_length=2,
+        max_length=5,
+        regex="^[a-zA-Z]+$",
+        title="String Query",
+        example="abc",
+    ),
+    number_query: float = Query(
+        default=1.0, ge=0.5, le=10.5, title="Number Query", example=5.5
+    ),
+):
+    return {"string_query_handled": string_query, "number_query_handled": number_query}
