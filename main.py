d21e02629940932fa9305fac31bfbd5403554412
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 15:46:07 +0900

    pydantic 중첩 모델

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: str
+    name: str
+
+
 class Item(BaseModel):
-    name: str = Field(..., title="Item Name", min_length=2, max_length=50)
-    description: str = Field(
-        None, description="The description of the item", max_length=300
-    )
-    price: float = Field(..., gt=0, description="The price must be greater than zero")
-    tag: List[str] = Field(default=[], alias="item-tags")
+    name: str
+    description: str
+    image: Image
 
 
 @app.post("/items/")
-async def create_item(item: Item):
+def create_item(item: Item):
     return {"item": item.dict()}
