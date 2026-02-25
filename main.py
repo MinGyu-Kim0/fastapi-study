c401156593340108e2e88f0ed45efa3425b04a8c
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 11:16:11 +0900

    fastAPI 타입힌트 (List, Dict)

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import List, Dict
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/items/")
+# 타입은 List이어야 하고 각각의 요소는 int형이어야 함. List타입 힌트는 Query와 반드시 같이 사용해야함.
+async def read_items(q: List[int] = Query([])):
+    return {"q": q}
 
 
-@app.get("/getdata/")
-def read_items(data: str = "funcoding"):  # <- funcoding = 기본 값. 없어도 됨
-    return {"data": data}
+@app.post("/create-item/")
+async def create_item(item: Dict[str, int]):
+    return item
 
 
 # 실행: uvicorn main:app --reload
