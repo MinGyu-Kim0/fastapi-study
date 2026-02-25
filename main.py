bb6daf32f36f0ac81ea0e0b4b8785026889282fc
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 20:12:13 +0900

    jinja2 템플릿 상속

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from typing import List
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/dynamic_items/")
-def dynamic_items(request: Request, item_list: str = ""):
-    items = item_list.split(",")
+@app.get("/inherit")
+def template_inherit(request: Request):
+    my_text = "FastAPI와 Jinja2를 이용한 예시입니다."
     return templates.TemplateResponse(
-        "index.html", {"request": request, "items": items}
+        "index.html", {"request": request, "text": my_text}
     )
