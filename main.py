29801693ea579bd13857b1553caefc056feb899f
MINGYUKIM <coinmagic8085@gmail.com>
2026-02-25 19:57:29 +0900

    jinja2 템플릿 조건문

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    try:
-        if item_id < 0:
-            raise ValueError("음수는 허용되지 않습니다.")
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/items2/{item_id}")
-def read_item(item_id: int):
-    if item_id == 42:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"item_id": item_id}
+@app.get("/greet")
+def greeting(request: Request, time_of_day: str):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "time_of_day": time_of_day}
+    )
